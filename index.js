0ad7582e1c24efa5ba0dc3f646625dd680a31a06
Matt Martinez <matt@Matts-MacBook-Pro.local>
2022-08-02 00:24:49 -0700

    added packagejson

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const express = require('express');
 const { join } = require('path');
 const path = require('path');
 const { uid } = require('uid');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'ecommerce_db'
+})
+
+const PORT = process.env.PORT || 3001;
 const app = express();
-const db = sg
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(join(__dirname, 'public')));
@@ -61,7 +70,7 @@ function menuStart() {
 };
 
 function viewAllDepartments() {
-  db.query('SELECT * FROM department', (err, res) => {
+  db.query('SELECT * FROM departments', (err, res) => {
     if (err) throw err;
     console.Table(res);
     menuStart();
@@ -85,19 +94,83 @@ function viewAllEmployees() {
 };
 
 function addDepartment() {
-
+  inquirer.prompt([
+    {
+      message: "Enter Department Name",
+      name: "departmentID",
+      type: "input"
+    }
+  ]).then(function (answer) {
+    db.query("INSERT INTO departments (departmentID) VALUEs")
+  })
 };
 
 function addRole() {
-
+  inquirer.prompt([
+    {
+      message: "Enter Role Name:",
+      type: "input",
+      name: "roleID",
+    },
+    {
+      message: "Enter Role Salary",
+      type: "input",
+      name:"salary",
+    },
+    {
+      message: "Enter Role Department",
+      type: "input",
+      name: "departmentID",
+    }
+  ]).then()
 };
 
 function addEmployee() {
+  inquirer.prompt([
+    {
+      message: "Enter Employee first name:",
+      type: "input",
+      name: "first_name",
+    },
+    {
+      message: "Enter Employee last name:",
+      type: "input",
+      name: "last_name",
+    },
+    {
+      message: "Enter Employee role:",
+      type: "input",
+      name: "roleID",
+    },
+    {
+      message: "Enter Manager ID:",
+      type: "input",
+      name: "managerID",
+    },
+  ])
+  .then(function (answer) {
 
+  })
 };
 
 function updateRole() {
-  
+  inquirer.prompt([
+    {
+      message: "Enter Employee ID:",
+      type: "input",
+      name: "employeeID",
+    },
+    {
+      message: "Enter New Role ID:",
+      type: "input",
+      name: "roleID",
+    },
+
+  ]).then(update => {
+    let newRole = {
+      role
+    }
+  })
 };
 
 menuStart();
